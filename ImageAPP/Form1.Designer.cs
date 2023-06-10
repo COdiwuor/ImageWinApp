62d372a606c0bc15f1a4c54a7512567a3c6ce0db
COdiwuor <cyrilondime@gmail.com>
2023-06-10 22:01:02 +0300

    latest changes

--- ImageAPP/Form1.Designer.cs
+++ ImageAPP/Form1.Designer.cs
@@ -143,7 +143,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(173, 23);
             this.button5.TabIndex = 9;
-            this.button5.Text = "Save";
+            this.button5.Text = "Download Image";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
